1635c71368757463598d000ea4b9102b634fa46f
Athena Iakovidi <athena.iak@gmail.com>
2017-06-26 23:17:29 +0200

    Modify mosaic script to process all movies of one day and output a mat file for CNMF-E

--- mosaic_script.m
+++ mosaic_script.m
@@ -2,23 +2,31 @@ clc; clear; close all;
 
 srcDir = '~/tp/athina/Mosaic-MATLAB/src';
 inDir = '~/tp/athina/data/4294/';
-outDir = '~/tp/athina/data/4294/output/20170623_of_20160531_143408';
+outDir = '~/tp/athina/data/4294/output/20170626_of_20160531';
 
-% movieFiles = {...
-%     %'recording_20160118_140521.xml', ...
-%     'recording_20160125_114209.xml'...
-% };
-
+outFileName = 'ready_20160531.mat';
 movieFiles = {...
     'recording_20160531_141257.tif', ...
-    %'recording_20160531_141257-001.tif', ...
-    %'recording_20160531_141257-002.tif', ...
-    %failed to correct motion:
-    %'recording_20160531_143408.tif',
-    %'recording_20160531_143408-001.tif',
-    %'recording_20160531_143408-002.tif',
-    %'recording_20160531_143408-003.tif',
-    %'recording_20160531_143408-004.tif'
+    'recording_20160531_141257-001.tif', ...
+    'recording_20160531_141257-002.tif', ...
+    'recording_20160531_141829.tif', ...
+    'recording_20160531_141829-001.tif', ...
+    'recording_20160531_141829-002.tif', ...
+    'recording_20160531_141829-003.tif', ...
+    'recording_20160531_141829-004.tif', ...
+    'recording_20160531_142630.tif', ...
+    'recording_20160531_142630-001.tif', ...
+    'recording_20160531_142630-002.tif', ...
+    'recording_20160531_142630-003.tif', ...
+    'recording_20160531_142630-004.tif', ...
+    'recording_20160531_143408.tif', ...
+    'recording_20160531_143408-001.tif', ...
+    'recording_20160531_143408-002.tif', ...
+    'recording_20160531_143408-003.tif', ...
+    'recording_20160531_143408-004.tif', ...
+    'recording_20160531_144143.tif', ...
+    'recording_20160531_144143-001.tif', ...
+    'recording_20160531_144143-002.tif'
 };
 
 % crop parameters
@@ -50,52 +58,37 @@ addpath(srcDir);
 prefs = mosaic.Preferences(prefsFile, workDir, memoryQuota, driveQuota);
 mosaic.initialize('preferences', prefs);
 
-%% load tiff movies
-
-movies = mosaic.List('mosaic.Movie');
+%% motion correct each movie
 for m = 1:numMovies
+    % load movie
     movie = mosaic.loadMovieTiff(fullfile(inDir, movieFiles{m}));
-    movies.add(movie);
-end
-clear movie;
-
-%% crop movies
-
-croppedMovies = mosaic.List('mosaic.Movie');
-for m = 1:numMovies
-    croppedMovie = mosaic.cropMovie(movies.get(m), ...
+    
+    % crop movie
+    croppedMovie = mosaic.cropMovie(movie, ...
         crop(1), crop(2), crop(3), crop(4), 'coordinateSystem', 'pixels');
-    croppedMovies.add(croppedMovie);
-end
-clear movies croppedMovie;
-
-%% Preprocess the data to reduce data volume and remove artifacts.
-ppMovies = mosaic.List('mosaic.Movie');
-for m = 1:numMovies
-    ppMovie = mosaic.preprocessMovie(croppedMovies.get(m), ...
+    clear movie;
+    
+    % preprocess movie
+    ppMovie = mosaic.preprocessMovie(croppedMovie, ...
        'fixDefectivePixels', true, ...
        'fixRowNoise', false, ...
        'fixDroppedFrames', false, ...
        'spatialDownsampleFactor', 1); % do not downsample
-    ppMovies.add(ppMovie);
-    %movName = fullfile(outDir, sprintf('preproc_%s.tif',croppedMovies.get(m).getName()));
-    %mosaic.saveMovieTiff(ppMovie, movName, 'compression', 'None');
-end
-clear ppMovie movName croppedMovies;
-
-%% concatenate movie files to one file
-concatMovie = mosaic.concatenateMovies(ppMovies, ...
-    'gapType', 'Add one time period between movies');
-clear ppMovies;
-
-%% Correct motion in each movie.
-
-%mcMovies = mosaic.List('mosaic.Movie');
-referenceFrame = mosaic.extractFrame(concatMovie, 'frame', 1);
-mcRoi = mosaic.RectangleRoi(mosaic.Point(roi(1), roi(2)), mosaic.Point(roi(3), roi(4)));
-%mcRoi.view(concatMovie);
-
-[mcMovie, translations] = mosaic.motionCorrectMovie(concatMovie, ... 
+    clear croppedMovie;
+    
+    % motion correct movie
+    % - define the reference frame
+    refName = fullfile(outDir,'referenceFrame.tif');
+    if m==1
+        referenceFrame = mosaic.extractFrame(ppMovie, 'frame', 1);
+    else
+        referenceFrame = mosaic.loadImage(refName);
+    end
+    
+    mcRoi = mosaic.RectangleRoi(mosaic.Point(roi(1), roi(2)), mosaic.Point(roi(3), roi(4)));
+    %mcRoi.view(concatMovie);
+    
+    [mcMovie, translations] = mosaic.motionCorrectMovie(ppMovie, ... 
     'referenceImage', referenceFrame, ...
     'motionType', 'Translation', ... 
     'roi', mcRoi, ... 
@@ -109,66 +102,69 @@ mcRoi = mosaic.RectangleRoi(mosaic.Point(roi(1), roi(2)), mosaic.Point(roi(3), r
     'applySpatialMeanPixels', 5, ... 
     'minimumValue', -102, ... 
     'maximumValue', 108, ... 
-    'autoCrop', true);
-clear concatMovie;
+    'autoCrop', false ... % true ...
+    );
+    clear ppMovie;
     
-movName = fullfile(outDir, sprintf('mcorr_%s.tif',movieFiles{1}));
-mosaic.saveMovieTiff(mcMovie, movName, 'compression', 'None');
+    % save motion corrected movie
+    movName = fullfile(outDir, sprintf('mcorr_%s', movieFiles{m}));
+    mosaic.saveMovieTiff(mcMovie, movName, 'compression', 'None');
+    
+    % save the last frame as a reference for next part of the movie
+    idx = mcMovie.getTimingInfo().getNumTimes();
+    lastFrame = mosaic.extractFrame(mcMovie, 'frame', idx);
+    mosaic.saveImageTiff(lastFrame, refName,'compression','None');
+    
+    clear mcMovie;
+end
 
-clear concatMovie movName;
+clear referenceFrame idx translations;
+%% load all motion corrected movies and concatenate movie them
+% load tiff movies
+movies = mosaic.List('mosaic.Movie');
+for m = 1:numMovies
+    movie = mosaic.loadMovieTiff(fullfile(outDir, sprintf('mcorr_%s', movieFiles{m})));
+    movies.add(movie);
+end
+clear movie;
+
+concatMovie = mosaic.concatenateMovies(movies, ...
+    'gapType', 'Add one time period between movies');
+clear ppMovies;
 
 %% Temporally downsample and save as .mat for CNMF-E
-currentStep = mcMovie.getTimingInfo().getStep();
+currentStep = concatMovie.getTimingInfo().getStep();
 desiredStep = 0.2; % CNMF wants 5 Hz data input
 downsamplingFactor = round(desiredStep / currentStep);
-Y = mcMovie.getData();
-Y = Y(:,:,1:downsamplingFactor:end);
 
+downsMovie = mosaic.resampleMovie(concatMovie, ...
+    'spatialReduction', 1, ...
+    'temporalReduction', downsamplingFactor);
+
+Y = downsMovie.getData();
+%Y = Y(:,:,1:downsamplingFactor:end);
+
+% remove unwanted rows (caused by motion correction)
+% 1. sum over columns if sum=0, then the row is blank and it should not be
+%used (the motion correction algorithm made it blank).
+% 2. find the minimum sub over columns for each row, over all frames (even
+% if only one frame does not contain data for this row, we cannot use the
+% whole row.
+% rule: min(sum(Y,2),[],3)==0;
+% remove unwanted columns, rule: min(sum(Y,1),[],3)==0;
+
+% keep only rows and columns that never had sum=0.
+Y = Y(find(min(sum(Y,2),[],3)~=0),find(min(sum(Y,1),[],3)~=0),:);
+
+% save some meta-data
 Ysiz = size(Y)';
 Yfs = 1/desiredStep;
 
-nam_mat = fullfile(outDir, sprintf('ready_%s.mat',movieFiles{1}));
+nam_mat = fullfile(outDir, outFileName);
 save(nam_mat, 'Y', 'Ysiz', 'Yfs', '-v7.3');
 
 clear Y Ysiz Yfs;
 clear currentStep desiredStep downsamplingFactor;
 
-%% Normalize the movie by the mean frame (df/f).
-dffMovie = mosaic.normalizeMovie(mcMovie, 'method', '(f-f0)/f0');
-clear mcMovie;
-%dffMovie.view()
-
-dffMovieFile = fullfile(outDir, 'processedMovie.mat');
-%mosaic.saveOneObject(dffMovie, dffMovieFile);
-
-%% Identify cells using PCA-ICA.
-
-ics = mosaic.pcaIca(dffMovie, 'unmix', 'traces', ...
-    'numPCs', 350, 'numICs', 270);
-% save object to view later
-icsFile = fullfile(outDir, 'processedMovie-ICs.mat');
-mosaic.saveOneObject(ics, icsFile);
-
-% view traces etc
-icTraces = ics.getList('types', {'mosaic.Trace'});
-% icTraces.get(1).view()
-icMixingImages = ics.getMixingImages();
-icUnmixingImages = ics.getUnmixingImages();
-% icMixingImages.get(1).view()
-
-%% Detect events in IC traces.
-eventTraces = mosaic.List('mosaic.Trace');
-for i = 1:icTraces.getSize()
-    eventTrace = mosaic.detectEvents(icTraces.get(i));
-    eventTraces.add(eventTrace);
-end
-% eventTraces.get(1).view()
-
-%% Save the events.
-mosaic.saveList(eventTraces, eventsFile);
-
-%% Load a previous list of traces
-eventsFile = fullfile(outDir, 'processedMovie-ICs-events.mat');
-eventTraces = mosaic.loadObjects(eventsFile).getList();
 %% Terminate the Mosaic script session.
 mosaic.terminate();
