c070c57dce513ebd2f5c8f7589526dd7a2e72414
Athena Iakovidi <athena.iak@gmail.com>
2017-11-10 17:12:28 +0100

    Found one bug (no current fix). Tried 3 new metrics but they didn't work

--- bad_cell_removal/ideal_comparison.m
+++ bad_cell_removal/ideal_comparison.m
@@ -3,56 +3,62 @@ function [ error ] = ideal_comparison( neur, doPlot )
 %given spatial footprint (ideal shape) and calculates the mean squared 
 %error between the two matrices.
 % Input:
-%   neur    :   2-Dimensional spatial footprint
+%   neur    :   2-Dimensional spatial footprint (assumes that boundaries
+%   are cropped)
 %   doPlot  :   if set true, creates a plot of the ideal spatial footprint
 % Output: 
 %   error   :   the minimum squared error of all pixels compared with an
 %   ideal shape of the neuron    
 
-    % new method:
-    [x,y] = ind2sub(size(neur),find(neur>0));
-    cropped_neur = neur(min(x):max(x),min(y):max(y));
-    neur1d = reshape(cropped_neur,1,[]);
-    
-    % normalize the spatial footprint (0-1)
-    %neur1d = neur1d/max(neur1d);
-    ccc = normcdf(neur1d,mean(neur1d),std(neur1d));
-    ccc(find(ccc==min(ccc)))=0;
-    neur1d = neur1d/max(neur1d);
-    error2 = sqrt(sum(bsxfun(@minus,neur1d,ccc).^2,2));
-    
-    % old method:
-    % clear some noise
-    neurClean = zeros(size(neur));
-    neurClean(find(neur>max(max(neur))/5)) = neur(find(neur>max(max(neur))/5));
-
-    
-    %noiseLevel = mean(neur1d(find(neur1d>0)))-3*std(neur1d(find(neur1d>0)))
-%neurClean(find(neur2d>noiseLevel)) = neur2d(find(neur2d>noiseLevel));
-    [x,y] = ind2sub(size(neurClean),find(neurClean>0));
-    neurClean = neurClean(min(x):max(x),min(y):max(y)); % crop blank boundaries
-
-    ideal = zeros(size(neurClean));
-    gSiz = min(size(neurClean));
+    % create a square gaussian filter (almost) the same dimensions 
+    ideal = zeros(size(neur));
+    gSiz = min(size(neur));
     rowStart = floor((size(ideal,1) - gSiz)/2) + 1;
     colStart = floor((size(ideal,2) - gSiz)/2) + 1;
-
     ideal(rowStart:rowStart+gSiz-1,colStart:colStart+gSiz-1) = ...
-     fspecial('gaussian', gSiz, gSiz/4);
-    [realCenterVal,~] = max(max(neurClean));
+     fspecial('gaussian', gSiz, gSiz/3);
+
+    % normalize the spatial footprint's values to be in the 0-1 range
+    [realCenterVal,~] = max(max(neur));
     realNormalFactor = 1/realCenterVal;
-    neurClean = neurClean * realNormalFactor;
-    gaussianCenterVal = ideal(rowStart+round(gSiz/2),colStart+round(gSiz/2));
-    gaussNormalFactor = 1/gaussianCenterVal;
-    ideal = ideal * gaussNormalFactor;
-
-    neur1d = reshape(neurClean,1,[]);
-    ideal1d = reshape(ideal,1,[]);
-    error = sqrt(sum(bsxfun(@minus,neur1d,ideal1d).^2,2));
-
+    neur = neur * realNormalFactor;
+    
+    % I don't remember how I decided this. It works pretty well, but it 
+    % doesn't seem to have any mathematical premise
+    %
+    %gaussianCenterVal = ideal(rowStart+round(gSiz/2),colStart+round(gSiz/2));
+    %gaussNormalFactor = 1/gaussianCenterVal;
+    %ideal = ideal * gaussNormalFactor;
+    %
+    % so, after thinking about this, I think this was the rationale:
+    % We optimize the gaussian filter to be as similar as possible to the
+    % hgihest value ('centroid') of the spatial footprint. In that way, we
+    % minimize error that comes from the higher values and we depend more
+    % on errors away from the center. I think at the end, what this metric
+    % really measured was the distance of the centroid from the center of
+    % the picture (by dividing the idea with the centroid value, I pushed 4
+    % values above the zero. One of them was above the centroid and so had
+    % error=0, the other 3 were symmetrical to the centroid. The farther 
+    % away the centroid was from the center of the picture, the higher the
+    % error in the other 3 points. Although it kinda worked, I don't like
+    % how arbitrary this metric is. But something that utilizes the
+    % information about the centroid may make sense.
+    
+    % without the previous lines, it doesn't work at all
+    %neur1d = reshape(neur,1,[]);
+    %ideal1d = reshape(ideal,1,[]);
+    %error = sqrt(sum(bsxfun(@minus,neur1d,ideal1d).^2,2));
+    % ^ oime, i don't take the mean for the error D:D:D:D:
+    
+    % try 3 more metrics
+    peaksnr = psnr(neur,ideal);
+    ssimval = ssim(neur,ideal) ;
+    error = immse(neur,ideal);
+    % none worked!
+    
     if doPlot
         imagesc(ideal);
-        title(sprintf('Ideal err: %.2f, new: %.2f', error,error2));
+        title(sprintf('n:%.2f s:%.4f e:%.3f', peaksnr, ssimval, error));
     end
 end
 
